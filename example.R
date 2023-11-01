1697115bfa48d3420d4ca9db3335fb5cfc92940e
Zhuoran Li <zhl4010@med.cornell.edu>
2023-11-01 13:18:13 -0400

    change data

--- example.R
+++ example.R
@@ -4,6 +4,7 @@ devtools::install_github("Zhuoran-Li123/BoxplotPackage", force = TRUE)
 # load the 'BoxplotPackage' package
 library(BoxplotPackage)
 # load the dataset "DRG_data" from the 'BoxplotPackage' package
-data = BoxplotPackage::DRG_data
+library(readr)
+DRG_data <- read_csv("data/DRG_data.csv")
 # create a boxplot by boxplot_payment function
 boxplot_payment(DRG_data, 'Average Covered Charges')
